91fc1ed7d473fe8862a6233d22a1b0bac5285252
Ryan Womack <ryan@ryanwomack.com>
2017-05-23 12:17:03 -0400

    ggvis

--- R_for_DataLiteracy.R
+++ R_for_DataLiteracy.R
@@ -86,4 +86,24 @@ GraphIt<-function(a,b)
 
 }
 
+# ggvis
+library(ggvis)
+
+diamonds %>% ggvis(~carat, ~price, fill=~clarity)
+
+diamonds %>% 
+  ggvis(~carat) %>%
+  layer_densities(
+    adjust = input_slider(.1, 2, value = 1, step = .1, label = "Bandwidth adjustment"),
+    kernel = input_select(
+      c("Gaussian" = "gaussian",
+        "Epanechnikov" = "epanechnikov",
+        "Rectangular" = "rectangular",
+        "Triangular" = "triangular",
+        "Biweight" = "biweight",
+        "Cosine" = "cosine",
+        "Optcosine" = "optcosine"),
+      label = "Kernel")
+  )
+
 # use app.R file and PopulationPyramid.R file for the remainder of the exercise
