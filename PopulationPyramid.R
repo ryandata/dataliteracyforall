0c2c5ecd776fff352edc3ae23361712ac9ed0434
Ryan Womack <ryan@ryanwomack.com>
2017-05-23 11:41:41 -0400

    first commit

---
+++ PopulationPyramid.R
@@ -0,0 +1,64 @@
+library(idbr) # devtools::install_github('walkerke/idbr')
+library(ggplot2)
+library(animation)
+library(dplyr)
+library(ggthemes)
+
+setwd("/home/ryan/Desktop/R_for_DataLiteracy/github")
+# idb_api_key("Your Census API key goes here")
+# get your key at
+# http://api.census.gov/data/key_signup.html
+
+idb_api_key('0c48470badbfdfbb0475940b6374c09f2476720f')
+
+countrycode<-c('MG')
+countryname<-"Mongolia"
+startyear=2010
+endyear=2050
+
+
+
+male <- idb1(countrycode, startyear:endyear0c48470badbfdfbb0475940b6374c09f2476720f, sex = 'male') %>%
+  mutate(POP = POP * -1,
+         SEX = 'Male')
+
+female <- idb1(countrycode, startyear:endyear, sex = 'female') %>%
+  mutate(SEX = 'Female')
+
+countrydata <- rbind(male, female) %>%
+  mutate(abs_pop = abs(POP))
+
+# Animate it with a for loop
+
+saveGIF({
+
+  for (i in startyear:endyear) {
+
+    title <- as.character(i)
+
+    year_data <- filter(countrydata, time == i)
+
+    g1 <- ggplot(year_data, aes(x = AGE, y = POP, fill = SEX, width = 1)) +
+      coord_fixed() +
+      coord_flip() +
+      annotate('text', x = 98, y = -800000,
+               label = 'Data: US Census Bureau IDB; idbr R package', size = 3) +
+      geom_bar(data = subset(year_data, SEX == "Female"), stat = "identity") +
+      geom_bar(data = subset(year_data, SEX == "Male"), stat = "identity") +
+      scale_y_continuous(breaks = seq(-1000000, 1000000, 500000),
+                         labels = paste0(as.character(c(seq(1, 0, -0.5), c(0.5, 1))), "m"),
+                         limits = c(min(countrydata$POP), max(countrydata$POP))) +
+      theme_economist(base_size = 14) +
+      scale_fill_manual(values = c('#ff9896', '#d62728')) +
+      ggtitle(paste0('Population structure of ',countryname, title)) +
+      ylab('Population') +
+      xlab('Age') +
+      theme(legend.position = "bottom", legend.title = element_blank()) +
+      guides(fill = guide_legend(reverse = TRUE))
+
+    print(g1)
+
+  }
+
+}, movie.name = 'pyramid.gif', interval = 0.1, ani.width = 700, ani.height = 600)
+
---
+++ R_for_DataLiteracy.R
@@ -0,0 +1,89 @@
+###############################
+# Data Literacy for All, with R
+# IASSIST Workshop
+# Ryan Womack
+# 2017-05-23
+###############################
+
+
+
+#install required packages for this session
+install.packages("WDI", dependencies=TRUE)
+install.packages("googleVis", dependencies=TRUE)
+install.packages("lattice", dependencies=TRUE)
+install.packages("ggplot2", dependencies=TRUE)
+install.packages("ggvis", dependencies=TRUE)
+install.packages("reshape2", dependencies=TRUE)
+install.packages("tidyverse", dependencies=TRUE)
+install.packages("devtools", dependencies=TRUE)
+install.packages("animation", dependencies=TRUE)
+install.packages("ggthemes", dependencies=TRUE)
+install.packages("dplyr", dependencies=TRUE)
+install.packages("gridExtra", dependencies=TRUE)
+
+
+# replace with your directory name!
+setwd("/home/ryan/Desktop/R_for_DataLiteracy/github")
+
+# now create a New Package using menus in RStudio
+
+# explore some data with WDI package
+
+library(WDI)
+#google to find these codes :)
+countries<-c("US","CN","GB","DE","MN","RU","CA","AU")
+indicators<-c("NY.GDP.PCAP.KD","SP.URB.TOTL.IN.ZS","SP.RUR.TOTL.ZS","ST.INT.ARVL")
+realnames<-c("iso2c","country","year","PerCapitaGDP_Constant","Urban_%","Rural_%","Tourism_Arrivals")
+mydata<-WDI(country=countries, indicator=indicators, start=1996, end=2015)
+names(mydata)<-realnames
+library(lattice)
+attach(mydata)
+xyplot(PerCapitaGDP_Constant~year, groups=country, auto.key=list(space="right"))
+library(ggplot2)
+ggplot(data=mydata, aes(x=year, y=PerCapitaGDP_Constant,group=country, colour=country)) +
+  geom_line()
+
+# save the data ...
+library(devtools)
+use_data(mydata)
+
+# alternatively, we could find a .csv file and import with
+# mydata <- read.csv("myfile.csv")
+# and save the data...
+
+# functions
+
+# a basic function
+myfunction<-function(x,y)
+{
+  x+y+200
+}
+
+#save this as myfunction.R into the R directory of your package
+
+library(ggplot2)
+library(lattice)
+data(diamonds)
+attach(diamonds)
+
+# comparison functions
+CompareIt<-function(a,b)
+{
+  cat("Median ",median(a)," Mean ",mean(a))
+  cat("Median ",median(b)," Mean ",mean(b))
+}
+
+GraphIt<-function(a,b)
+{
+  plot_a<-bwplot(a)
+  plot_b<-bwplot(b)
+  plot_c<-densityplot(a)
+  plot_d<-densityplot(b)
+  print(plot_a, split = c(1, 1, 2, 2), more = TRUE)
+  print(plot_b, split = c(2, 1, 2, 2), more = TRUE)
+  print(plot_c, split = c(1, 2, 2, 2), more = TRUE)
+  print(plot_d, split = c(2, 2, 2, 2), more = FALSE)  # more = FALSE is redundant
+
+}
+
+# use app.R file and PopulationPyramid.R file for the remainder of the exercise
